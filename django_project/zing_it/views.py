be9b80e3f212af3b792c1642068431fa77c8e1ba
py2fun <developer@py2fun.com>
2024-03-27 09:36:21 +0000

    add branch music-types

--- django_project/zing_it/views.py
+++ django_project/zing_it/views.py
@@ -4,15 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    biggest_hits_2018 = [
-            {"id": 1, "Track": "告黑气球", "Artist": "周伦", "Album": "爱的第七章", "Length": "3:27"},
-            {"id": 2, "Track": "大酒窝", "Artist": "蔡俊杰, 林卓妍", "Album": "JJ陆", "Length": "3:34"},
-            {"id": 3, "Track": "Better Now", "Artist": "Post Malone", "Album": "beerbongs & bentleys", "Length": "3:51"},
-            {"id": 4, "Track": "第一次", "Artist": "光优", "Album": "第一次个人创作专辑", "Length": "3:04"},
-            {"id": 5, "Track": "冰雨", "Artist": "德华", "Album": "Pop", "Length": "3:21"},
-            {"id": 6, "Track": "一起长大的约定", "Artist": "周伦", "Album": "我很忙", "Length": "3:14"},
-        ]
-    return render(request,'zing_it/home.html',{"songs": biggest_hits_2018})
+    music_types=['Pop','Rock','R&B','Soul & Funk','Blues','Reggae','Soundtracks','Dance & EDM','Rap', 'Asian Music','Jazz','Kpop','Metal','Electronic','Classical']
+    return render(request,'zing_it/home.html',{"music_types":music_types})
 
 
 def about(request):
